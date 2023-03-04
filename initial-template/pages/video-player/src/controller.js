0d8a61a703f9eb3dd02ccbaedf3effb9d597f3b6
Marco Martins <marcoamj1982@gmail.com>
2023-03-04 17:37:39 -0300

    class1

---
+++ initial-template/pages/video-player/src/controller.js
@@ -0,0 +1,70 @@
+export default class Controller {
+    #view
+    #worker
+    #blinkCounter = 0
+    #camera
+
+
+    constructor({ view, worker, camera }) {
+        this.#view = view
+        this.#camera = camera
+        this.#worker = this.#configureWorker(worker)
+    }
+
+    static async initialize(deps) {
+        const controller = new Controller(deps)
+        controller.log('not yet detecting eye blink! click in the button to start')
+        return controller.init()
+    }
+
+    #configureWorker(worker) {
+        let ready = false
+        worker.onmessage = ({ data }) => {
+            if ('READY' === data) {
+                this.#view.enableButton()
+                ready = true
+                console.log('Camera worker is ready');
+                return;
+            }
+
+            const { blinked } = data
+            if (!blinked) return;
+
+            this.#blinkCounter ++
+            this.log('you blinked eyes')
+            this.#view.togglePlayVideo();
+            console.log('blinked', blinked);
+        }
+        return {
+            send(msg) {
+                if (!ready) return;
+                worker.postMessage(msg)
+            }
+        }
+    }
+
+    async init() {
+        console.log('Run Video Player');
+        this.#view.configureOnBtnClick(this.onBtnStart.bind(this))
+    }
+
+    log(text) {
+        const times = `      - blinked times: ${this.#blinkCounter}`
+        this.#view.log(`logger: ${text}`.concat(this.#blinkCounter ? times : ''))
+    }
+
+    onBtnStart() {
+        this.log('initializing detection ...')
+        this.#blinkCounter = 0
+        this.loop()
+    }
+
+    loop() {
+        const video = this.#camera.video
+        const img = this.#view.getVideoFrame(video)
+        this.#worker.send(img)
+        setTimeout(() => this.loop(), 100);
+    }
+
+
+}
