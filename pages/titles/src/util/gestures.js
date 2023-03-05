35ba9db991b5c011edeac7f6a7c93f907abaed30
Marco Martins <marcoamj1982@gmail.com>
2023-03-05 12:55:46 -0300

    desafio aula 2

--- pages/titles/src/util/gestures.js
+++ pages/titles/src/util/gestures.js
@@ -2,6 +2,12 @@ const { GestureDescription, Finger, FingerCurl } = window.fp;
 
 const ScrollDownGesture = new GestureDescription('scroll-down') // ✊️
 const ScrollUpGesture = new GestureDescription('scroll-up') // 🖐
+const LoveYouGesture = new GestureDescription('love-you') // 🤟
+
+LoveYouGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0)
+LoveYouGesture.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 0.5)
+LoveYouGesture.addCurl(Finger.Index, FingerCurl.NoCurl, 1.0)
+LoveYouGesture.addCurl(Finger.Pinky, FingerCurl.NoCurl, 0.8)
 
 ScrollDownGesture.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 1.0)
 ScrollDownGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 0.5)
@@ -15,11 +21,20 @@ for (let finger of Finger.all) {
     ScrollUpGesture.addCurl(finger, FingerCurl.NoCurl, 1.0)
 }
 
-const knownGestures = [ScrollDownGesture, ScrollUpGesture]
+for (let finger of [Finger.Middle, Finger.Ring]) {
+    LoveYouGesture.addCurl(finger, FingerCurl.FullCurl, 1.0)
+}
+
+const knownGestures = [
+    ScrollDownGesture,
+    ScrollUpGesture,
+    LoveYouGesture
+]
 
 const gestureStrings = {
     'scroll-up': '🖐',
     'scroll-down': '✊️',
+    'love-you': '🤟',
 }
 
 export {
